772146ff5ab4c16210911b4790dd74a4c6b0f482
cascadetile <collent.credev@gmail.com>
2023-01-26 20:11:59 +0600

    feat: add ToggleWithText

---
+++ src/components/ToggleWithText/index.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './style.css';
+
+interface Props {
+  label: string
+  toggled: boolean
+  onClick: (value: boolean) => void
+}
+
+export const ToggleWithText: React.FC<Props> = ({ label, toggled, onClick }) => {
+  const [isToggled, toggle] = useState(toggled);
+
+  const callback = () => {
+    toggle(!isToggled);
+    onClick(!isToggled);
+  };
+
+  return (
+    <div className="toggle-with-text">
+      <span className="toggle-with-text__text">{label}</span>
+      <label className="toggle-with-text__label">
+        <input className="toggle-with-text__input" type="checkbox" defaultChecked={isToggled} onClick={callback} />
+        <span className="toggle-with-text__span" />
+      </label>
+    </div>
+  );
+};
