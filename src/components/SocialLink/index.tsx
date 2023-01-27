2ba82979c775db44a0336ae0c3c137d0744884ff
cascadetile <collent.credev@gmail.com>
2023-01-27 17:35:28 +0600

    feat: add SocialLink component

---
+++ src/components/SocialLink/index.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+import './style.css';
+
+interface Props {
+  link: string,
+  text: string
+}
+
+export const SocialLink: React.FC<Props> = ({ link, text }) => {
+  return <a href={link} className="social-link">{text}</a>;
+};
