84f671dad7198c523b848a6b7cbffaa742fb8fd8
cascadetile <collent.credev@gmail.com>
2023-01-28 23:03:56 +0600

    refactor: default export and omit bool YourAndMyTime

--- src/components/YourAndMyTime/index.tsx
+++ src/components/YourAndMyTime/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/function-component-definition */
+/* eslint-disable arrow-body-style */
 import React from 'react';
 import './style.css';
 import { Time } from '../Time';
@@ -5,8 +7,10 @@ import { Time } from '../Time';
 export const YourAndMyTime: React.FC = () => {
   return (
     <div className="your-and-my-time">
-      <Time isKiev={false} text="your time"></Time>
-      <Time isKiev={true} text="my time"></Time>
+      <Time isKiev={false} text="your time" />
+      <Time isKiev text="my time" />
     </div>
   );
 };
+
+export default YourAndMyTime;
