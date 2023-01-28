46055e929c6f3da7dcb16eea2d5117c7328eaf71
cascadetile <collent.credev@gmail.com>
2023-01-28 22:41:55 +0600

    refactor: default export in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/function-component-definition */
+/* eslint-disable arrow-body-style */
 import React from 'react';
 import './style.css';
 
@@ -9,3 +11,5 @@ export const Footer: React.FC = () => {
     </div>
   );
 };
+
+export default Footer;
