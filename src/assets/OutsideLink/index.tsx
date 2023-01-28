171340aa8caaea5e26da57edd81698644870ff06
cascadetile <collent.credev@gmail.com>
2023-01-28 22:38:09 +0600

    refactor: default export OutsideLink

--- src/assets/OutsideLink/index.tsx
+++ src/assets/OutsideLink/index.tsx
@@ -1,9 +1,13 @@
+/* eslint-disable react/function-component-definition */
+/* eslint-disable arrow-body-style */
 import React from 'react';
 
 export const OutsideLink: React.FC = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-      <path d="M3.6001 16.3996L16.4001 3.59961M16.4001 3.59961V16.3996M16.4001 3.59961L3.6001 3.59961" stroke="black"/>
+      <path d="M3.6001 16.3996L16.4001 3.59961M16.4001 3.59961V16.3996M16.4001 3.59961L3.6001 3.59961" stroke="black" />
     </svg>
   );
 };
+
+export default OutsideLink;
