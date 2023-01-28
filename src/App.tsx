295f9854ef23a1bfebb39f11e9f1f452d5147f15
cascadetile <collent.credev@gmail.com>
2023-01-28 22:22:44 +0600

    feat: set wrapper max-width to 1440px

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import { Footer } from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <AboutMe />
       <TimeAndLinks />
