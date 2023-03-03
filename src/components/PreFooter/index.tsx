7e8bb12f2ed49b25b699df43aab8ff923780f25b
cascadetile <collent.credev@gmail.com>
2023-03-03 22:47:31 +0600

    feat: add responsive 768px

--- src/components/PreFooter/index.tsx
+++ src/components/PreFooter/index.tsx
@@ -9,7 +9,9 @@ export const PreFooter: React.FC = () => {
   return (
     <div className="prefooter__wrapper">
       <div className="prefooter">
-        <YourAndMyTime />
+        <div className="prefooter__time-wrapper">
+          <YourAndMyTime />
+        </div>
         <div className="prefooter__title-wrapper">
           <div className="prefooter__title-1">Thanks for stopping by!</div>
           <div className="prefooter__title-2">If you like the work you’ve seen so far, let’s connect.</div>
