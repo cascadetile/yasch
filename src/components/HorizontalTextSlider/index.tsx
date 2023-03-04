bf06d8fad9804f1a9665a3ee85964f4c3fe6b1d5
cascadetile <collent.credev@gmail.com>
2023-03-04 23:28:47 +0600

    fix: remade horizontal text slider

--- src/components/HorizontalTextSlider/index.tsx
+++ src/components/HorizontalTextSlider/index.tsx
@@ -1,55 +1,18 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
-import React, { useEffect, useRef } from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
 import './style.css';
 
 export const HorizontalTextSlider: React.FC = () => {
-  const slideOne = useRef<HTMLSpanElement>(null);
-  const slideTwo = useRef<HTMLSpanElement>(null);
-
-  useEffect(() => {
-    if (slideOne.current) {
-      slideOne.current.addEventListener('animationiteration', () => {
-        if (slideOne.current && slideTwo.current) {
-          slideOne.current.style.animationPlayState = 'paused';
-          slideTwo.current.style.animationPlayState = 'paused';
-          setTimeout(() => {
-            if (slideOne.current && slideTwo.current) {
-              slideOne.current.style.animationPlayState = 'running';
-              slideTwo.current.style.animationPlayState = 'running';
-            }
-          }, 1000);
-        }
-      });
-    }
-  }, []);
-
   return (
-    <div className="horizontal-text-slider">
-      <p className="horizontal-text-slider__marquee">
-        <span ref={slideOne} className="horizontal-text-slider__span">
-          ux/ui
-          &nbsp;
-          <span className="horizontal-text-slider__designer">designer</span>
-          &nbsp;
-          ux/ui
-          &nbsp;
-          <span className="horizontal-text-slider__designer">designer</span>
-          &nbsp;
-        </span>
-      </p>
-      <p className="horizontal-text-slider__marquee horizontal-text-slider__marquee2">
-        <span ref={slideTwo} className="horizontal-text-slider__span">
-          ux/ui
-          &nbsp;
-          <span className="horizontal-text-slider__designer">designer</span>
-          &nbsp;
-          ux/ui
-          &nbsp;
-          <span className="horizontal-text-slider__designer">designer</span>
-          &nbsp;
-        </span>
-      </p>
+    <div className="horizontal-text-slider__wrapper">
+      <div className="horizontal-text-slider">
+        &nbsp;ux/ui <span className="horizontal-text-slider__designer">designer</span> ux/ui <span className="horizontal-text-slider__designer">designer</span>
+      </div>
+      <div className="horizontal-text-slider__two">
+        &nbsp;ux/ui <span className="horizontal-text-slider__designer">designer</span> ux/ui <span className="horizontal-text-slider__designer">designer</span>
+      </div>
     </div>
   );
 };
