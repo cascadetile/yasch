bc293a3877ebf60fd1a9273193b2cea225a85482
cascadetile <collent.credev@gmail.com>
2023-03-18 23:09:03 +0600

    feat: add dark theme in TimeAndLinks

--- src/components/TimeAndLinks/index.tsx
+++ src/components/TimeAndLinks/index.tsx
@@ -1,18 +1,22 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useContext } from 'react';
 import './style.css';
 import { YourAndMyTime } from '../YourAndMyTime';
 import { SocialLink } from '../SocialLink';
+import { ThemeContext } from '../../contexts';
 
 export const TimeAndLinks: React.FC = () => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="time-and-links">
-      <YourAndMyTime />
-      <div className="time-and-links__links">
-        <SocialLink link="https://www.linkedin.com/in/pinkaversa" text="LinkedIn" />
-        <SocialLink link="https://dribbble.com/PinkAversa" text="Dribbble" />
-        <SocialLink link="https://www.behance.net/aversa" text="Behance" />
+    <div className={`time-and-links__wrapper time-and-links__wrapper--${theme}`}>
+      <div className="time-and-links">
+        <YourAndMyTime />
+        <div className="time-and-links__links">
+          <SocialLink link="https://www.linkedin.com/in/pinkaversa" text="LinkedIn" />
+          <SocialLink link="https://dribbble.com/PinkAversa" text="Dribbble" />
+          <SocialLink link="https://www.behance.net/aversa" text="Behance" />
+        </div>
       </div>
     </div>
   );
