008cdb47997f993d9fca35441c8c2a9c2ef21a92
cascadetile <collent.credev@gmail.com>
2023-03-18 23:17:59 +0600

    feat: add dark theme in HorizontalTextSlider

--- src/components/HorizontalTextSlider/index.tsx
+++ src/components/HorizontalTextSlider/index.tsx
@@ -1,17 +1,19 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/jsx-one-expression-per-line */
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../contexts';
 import './style.css';
 
 export const HorizontalTextSlider: React.FC = () => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="horizontal-text-slider__wrapper">
+    <div className={`horizontal-text-slider__wrapper horizontal-text-slider__wrapper--${theme}`}>
       <div className="horizontal-text-slider">
-        &nbsp;ux/ui <span className="horizontal-text-slider__designer">designer</span> ux/ui <span className="horizontal-text-slider__designer">designer</span>
+        &nbsp;ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span>
       </div>
       <div className="horizontal-text-slider__two">
-        &nbsp;ux/ui <span className="horizontal-text-slider__designer">designer</span> ux/ui <span className="horizontal-text-slider__designer">designer</span>
+        &nbsp;ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span>
       </div>
     </div>
   );
