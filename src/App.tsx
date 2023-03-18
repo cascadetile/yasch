bd4645278ddc221acdb447c20ee65508af08929b
cascadetile <collent.credev@gmail.com>
2023-03-18 18:03:53 +0600

    feat: add ThemeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { createContext, useMemo, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Main } from './pages/Main';
 import { ResumePage } from './pages/ResumePage';
 
+interface IThemeContext {
+  theme: string
+  setTheme: React.Dispatch<React.SetStateAction<string>>
+}
+
+const ThemeContext = createContext<IThemeContext>({ theme: 'light', setTheme: () => {} });
+
 function App() {
+  const [theme, setTheme] = useState('light');
+  const themeMemo = useMemo(() => ({ theme, setTheme }), [theme]);
   return (
     <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/resume" element={<ResumePage />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeContext.Provider value={themeMemo}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/resume" element={<ResumePage />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
     </div>
   );
 }
