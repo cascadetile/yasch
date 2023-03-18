ac22b5e114926b555b2dd3011e19fe9cb03b6657
cascadetile <collent.credev@gmail.com>
2023-03-18 18:36:35 +0600

    feat: add dark theme to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,28 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './style.css';
 import { Logo } from '../../assets/Logo';
 import { Toggle } from '../Toggle';
+import { ThemeContext } from '../../contexts';
 
 interface IProps {
   hideMenu: boolean
 }
 
 export const Header: React.FC<IProps> = ({ hideMenu }) => {
+  const { theme, setTheme } = useContext(ThemeContext);
   const onToggleClick = (value: boolean) => {
-    console.log(value);
+    setTheme(value ? 'dark' : 'light');
   };
 
   return (
-    <div className="header__wrapper">
+    <div className={`header__wrapper header__wrapper--${theme}`}>
       <div className="header">
         <div>
           <Link to="/">
-            <Logo />
+            <Logo theme={theme === 'dark' ? 'white' : 'black'} />
           </Link>
         </div>
         <div className="header__menu">
@@ -50,7 +52,7 @@ export const Header: React.FC<IProps> = ({ hideMenu }) => {
           }
         </div>
         <div className="header__resume-and-toggle">
-          <div className="header__menu-item header__menu-item--resume">
+          <div className={`header__menu-item header__menu-item--resume header__menu-item--resume-${theme}`}>
             <NavLink
               className={({ isActive }) => { return isActive ? 'header__menu-item-link header__menu-item-link--active' : 'header__menu-item-link'; }}
               to="/resume"
