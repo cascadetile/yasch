e04b2554031c06006d238f8039f53c1a00a23c84
cascadetile <collent.credev@gmail.com>
2023-03-19 13:21:31 +0600

    feat: add dark theme in ResumeGroup

--- src/components/ResumeGroup/index.tsx
+++ src/components/ResumeGroup/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../contexts';
 import './style.css';
 
 interface IProps {
@@ -9,9 +10,10 @@ interface IProps {
 }
 
 export const ResumeGroup: React.FC<IProps> = ({ title, marginTop }) => {
+  const { theme } = useContext(ThemeContext);
   return (
     <div className="resume-group" style={{ marginTop: `${marginTop}px` }}>
-      <div className="resume-group__title">{title}</div>
+      <div className={`resume-group__title resume-group__title--${theme}`}>{title}</div>
     </div>
   );
 };
