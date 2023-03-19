70c451babf4b786530363cf07a05a4e5ce19576d
cascadetile <collent.credev@gmail.com>
2023-03-19 12:38:57 +0600

    feat: add dark theme in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../contexts';
 import './style.css';
 
 export const Footer: React.FC = () => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="footer">
+    <div className={`footer footer--${theme}`}>
       <div className="footer__left">Copyright © 2022 Vlad Yaschenkov </div>
       <div className="footer__right">All Rights Reserved.</div>
     </div>
