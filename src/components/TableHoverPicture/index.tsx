664c8d1093179aa0e52c43566d428c6df5e006b3
cascadetile <collent.credev@gmail.com>
2023-04-29 16:56:08 +0600

    fix: change bg color of work preview in dark theme

--- src/components/TableHoverPicture/index.tsx
+++ src/components/TableHoverPicture/index.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
-import React from 'react';
+import React, { useContext } from 'react';
 import './style.css';
+import { ThemeContext } from '../../contexts';
 
 interface IProps {
   imgUrl: string
 }
 
 export const TableHoverPicture: React.FC<IProps> = ({ imgUrl }) => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="table-hover-picture">
+    <div className={`table-hover-picture table-hover-picture--${theme}`}>
       <img className="table-hover-picture__img" src={imgUrl} alt="Project preview" />
     </div>
   );
