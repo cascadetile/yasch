36c7fbf2f4d9fd6f01ba707cd5c5e2e92372a897
cascadetile <collent.credev@gmail.com>
2023-04-30 20:37:36 +0600

    fix: don't cut stars in resume on big screens

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -28,57 +28,61 @@ how it has shaped the world we live in today.`;
 export const Resume: React.FC = () => {
   const { theme } = useContext(ThemeContext);
   return (
-    <div className="resume">
-      <div className="resume__star-large">
-        <StarLarge theme={theme === 'dark' ? '#373737' : '#CBCBCB'} />
-      </div>
-      <div className="resume__star-medium">
-        <StarMedium theme={theme === 'dark' ? '#373737' : '#CBCBCB'} />
-      </div>
-      <div className="resume__name-and-story">
-        <div>
-          <div className={`resume__name resume__name--${theme}`}>Vladislav Yaschenkov</div>
-          <div className={`resume__position resume__position--${theme}`}>UX/UI Designer</div>
+    <div className="resume-wrapper">
+      <div className="resume__star-wrapper">
+        <div className="resume__star-large">
+          <StarLarge theme={theme === 'dark' ? '#373737' : '#CBCBCB'} />
         </div>
-        <ResumeStoryOnMe />
+        <div className="resume__star-medium">
+          <StarMedium theme={theme === 'dark' ? '#373737' : '#CBCBCB'} />
+        </div>
       </div>
-      <div className="resume__groups">
-        <ResumeGroup title="Experience" marginTop={0} />
-        <ResumeGroupItem
-          title="co.dev"
-          subtitle="Brea, California · Remote"
-          jobRole="UX/UI Designer"
-          time="Sep 2022 – Present"
-          text={codev}
-        />
-        <ResumeGroupItem
-          title="Cultway"
-          subtitle="Berlin, Germany · Remote"
-          jobRole="UX/UI Designer"
-          time="Feb 2023 – Present"
-          text={cultway}
-        />
-        <ResumeGroupItem
-          title="H1 Agency"
-          subtitle="Lviv, Ukraine · Remote"
-          jobRole="UX/UI Designer"
-          time="Aug 2022 – Sep 2022"
-          text={h1agency}
-        />
-        <ResumeGroup title="Education" marginTop={64} />
-        <ResumeGroupItem
-          title="Kryvyi Rih State Pedagogical University"
-          time="Sep 2017 – Sep 2022"
-          text={education}
-        />
-        <ResumeGroup title="Hobbies" marginTop={64} />
-        <ResumeGroupItem
-          text={hobbies}
-        />
-        <div className="resume__links">
-          <SocialLink link="https://www.linkedin.com/in/pinkaversa" text="LinkedIn" />
-          <SocialLink link="https://dribbble.com/PinkAversa" text="Dribbble" />
-          <SocialLink link="https://www.behance.net/aversa" text="Behance" />
+      <div className="resume">
+        <div className="resume__name-and-story">
+          <div>
+            <div className={`resume__name resume__name--${theme}`}>Vladislav Yaschenkov</div>
+            <div className={`resume__position resume__position--${theme}`}>UX/UI Designer</div>
+          </div>
+          <ResumeStoryOnMe />
+        </div>
+        <div className="resume__groups">
+          <ResumeGroup title="Experience" marginTop={0} />
+          <ResumeGroupItem
+            title="co.dev"
+            subtitle="Brea, California · Remote"
+            jobRole="UX/UI Designer"
+            time="Sep 2022 – Present"
+            text={codev}
+          />
+          <ResumeGroupItem
+            title="Cultway"
+            subtitle="Berlin, Germany · Remote"
+            jobRole="UX/UI Designer"
+            time="Feb 2023 – Present"
+            text={cultway}
+          />
+          <ResumeGroupItem
+            title="H1 Agency"
+            subtitle="Lviv, Ukraine · Remote"
+            jobRole="UX/UI Designer"
+            time="Aug 2022 – Sep 2022"
+            text={h1agency}
+          />
+          <ResumeGroup title="Education" marginTop={64} />
+          <ResumeGroupItem
+            title="Kryvyi Rih State Pedagogical University"
+            time="Sep 2017 – Sep 2022"
+            text={education}
+          />
+          <ResumeGroup title="Hobbies" marginTop={64} />
+          <ResumeGroupItem
+            text={hobbies}
+          />
+          <div className="resume__links">
+            <SocialLink link="https://www.linkedin.com/in/pinkaversa" text="LinkedIn" />
+            <SocialLink link="https://dribbble.com/PinkAversa" text="Dribbble" />
+            <SocialLink link="https://www.behance.net/aversa" text="Behance" />
+          </div>
         </div>
       </div>
     </div>
