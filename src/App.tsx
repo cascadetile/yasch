2f1e0e14a9a5c3d78d43cde8cac7303577761c21
cascadetile <collent.credev@gmail.com>
2023-05-02 15:16:24 +0600

    feat: scroll to top when changing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { ScrollToTop } from './components/ScrollToTop';
 import { ThemeContext } from './contexts';
 import { Main } from './pages/Main';
 import { ResumePage } from './pages/ResumePage';
@@ -12,6 +13,7 @@ function App() {
     <div className={`app app--${theme}`}>
       <ThemeContext.Provider value={themeMemo}>
         <BrowserRouter>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/resume" element={<ResumePage />} />
---
+++ src/components/ScrollToTop/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/function-component-definition */
+
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;
