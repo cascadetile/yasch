f5474915dde3e796d8f9fb9b7ccb79a47745bae9
cascadetile <collent.credev@gmail.com>
2023-05-17 18:17:10 +0600

    feat: add email sending

--- src/components/PreFooter/index.tsx
+++ src/components/PreFooter/index.tsx
@@ -1,13 +1,36 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './style.css';
+import axios from 'axios';
 import { SocialLink } from '../SocialLink';
 import { YourAndMyTime } from '../YourAndMyTime';
 import { ThemeContext } from '../../contexts';
 
 export const PreFooter: React.FC = () => {
   const { theme } = useContext(ThemeContext);
+  const [emailInputVal, setEmailInputVal] = useState('');
+  const [isEmailInputDisabled, setIsEmailInputDisabled] = useState(false);
+  const sendEmail = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (emailInputVal !== '') {
+      try {
+        setIsEmailInputDisabled(true);
+        await axios({
+          method: 'POST',
+          url: 'https://mail-sender-api-n1zm.onrender.com/email',
+          data: {
+            email: emailInputVal,
+          },
+        });
+        setEmailInputVal('');
+      } catch (error) {
+        //
+      } finally {
+        setIsEmailInputDisabled(false);
+      }
+    }
+  };
   return (
     <div id="contacts" className={`anchor prefooter__wrapper prefooter__wrapper--${theme}`}>
       <div className="prefooter">
@@ -23,10 +46,22 @@ export const PreFooter: React.FC = () => {
           </div>
         </div>
         <div className="prefooter__email-and-links">
-          <form>
+          <form onSubmit={(e) => sendEmail(e)}>
             <div className="prefooter__email-form">
-              <input className={`prefooter__email-input prefooter__email-input--${theme}`} type="text" placeholder="Enter your email address" />
-              <button className={`prefooter__email-btn prefooter__email-btn--${theme}`} type="submit">Connect</button>
+              <input
+                value={emailInputVal}
+                onInput={(e) => setEmailInputVal((e.target as HTMLInputElement).value)}
+                className={`prefooter__email-input prefooter__email-input--${theme}`}
+                type="text"
+                placeholder="Enter your email address"
+              />
+              <button
+                disabled={isEmailInputDisabled}
+                className={`prefooter__email-btn prefooter__email-btn--${theme}`}
+                type="submit"
+              >
+                Connect
+              </button>
             </div>
           </form>
           <div className="prefooter__links">
