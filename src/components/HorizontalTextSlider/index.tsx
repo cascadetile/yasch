785ae602041ffd1b3d13cb8ea70b89fe41152e73
cascadetile <collent.credev@gmail.com>
2023-05-17 18:17:45 +0600

    fix: fix scroll behaviour on init render

--- src/components/HorizontalTextSlider/index.tsx
+++ src/components/HorizontalTextSlider/index.tsx
@@ -1,18 +1,24 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from '../../contexts';
 import './style.css';
 
 export const HorizontalTextSlider: React.FC = () => {
   const { theme } = useContext(ThemeContext);
+  const [textSliderClassName1, setTextSliderClassName1] = useState('');
+  const [textSliderClassName2, setTextSliderClassName2] = useState('');
+  useEffect(() => {
+    setTextSliderClassName1('horizontal-text-slider');
+    setTextSliderClassName2('horizontal-text-slider__two');
+  }, []);
   return (
     <div className={`horizontal-text-slider__wrapper horizontal-text-slider__wrapper--${theme}`}>
-      <div className="horizontal-text-slider">
+      <div className={textSliderClassName1}>
         &nbsp;ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span>
       </div>
-      <div className="horizontal-text-slider__two">
+      <div className={textSliderClassName2}>
         &nbsp;ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span>
       </div>
     </div>
