cf6e69b0ec70a31711030cbab0fc67c4233b3795
cascadetile <collent.credev@gmail.com>
2023-05-21 13:23:20 +0600

    fix: change dev link color when changing theme

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,7 +14,7 @@ export const Footer: React.FC = () => {
         <div className="footer__left-sm">© 2023</div>
       </div>
       <div className="footer__right">
-        Developed&nbsp;by: <a className="footer__right-link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/temirlan-aryngazy-900273256">Temirlan Aryngazy</a>
+        Developed&nbsp;by: <a className={`footer__right-link footer__right-link--${theme}`} target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/temirlan-aryngazy-900273256">Temirlan Aryngazy</a>
       </div>
     </div>
   );
