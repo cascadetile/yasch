37577bc8a63987434af0e7d5697fab23c4ea7722
cascadetile <collent.credev@gmail.com>
2023-05-27 16:23:25 +0600

    feat: make dedicated header for ResumePage

---
+++ src/components/ResumeHeader/index.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/function-component-definition */
+/* eslint-disable arrow-body-style */
+import React, { useContext } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './style.css';
+import { Logo } from '../../assets/Logo';
+import { Toggle } from '../Toggle';
+import { ThemeContext } from '../../contexts';
+
+export const ResumeHeader: React.FC = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+  const onToggleClick = (value: boolean) => {
+    setTheme(value ? 'dark' : 'light');
+  };
+
+  return (
+    <div className={`resume-header__wrapper header__wrapper--${theme}`}>
+      <div className="resume-header">
+        <div>
+          <Link to="/">
+            <Logo theme={theme === 'dark' ? 'white' : 'black'} />
+          </Link>
+        </div>
+        <div className="resume-header__menu" />
+        <div className="resume-header__resume-and-toggle">
+          <div className={`resume-header__menu-item resume-header__menu-item--resume resume-header__menu-item--resume-${theme}`}>
+            <NavLink
+              className={({ isActive }) => { return isActive ? 'resume-header__menu-item-link resume-header__menu-item-link--active' : 'resume-header__menu-item-link'; }}
+              to="/"
+            >
+              Go Home
+            </NavLink>
+          </div>
+          <Toggle toggled={theme === 'dark'} onClick={onToggleClick} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResumeHeader;
--- src/pages/ResumePage/index.tsx
+++ src/pages/ResumePage/index.tsx
@@ -2,14 +2,14 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
 import { Footer } from '../../components/Footer';
-import { Header } from '../../components/Header';
+import { ResumeHeader } from '../../components/ResumeHeader';
 import { PreFooter } from '../../components/PreFooter';
 import { Resume } from '../../components/Resume';
 
 export const ResumePage: React.FC = () => {
   return (
     <>
-      <Header hideMenu />
+      <ResumeHeader />
       <Resume />
       <PreFooter />
       <Footer />
