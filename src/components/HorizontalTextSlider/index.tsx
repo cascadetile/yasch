b62a6ff0d6581d8517c26bba2c63b63c6246bfe7
cascadetile <collent.credev@gmail.com>
2023-05-27 15:33:29 +0600

    fix: horizontal text scroller visible spawning

--- src/components/HorizontalTextSlider/index.tsx
+++ src/components/HorizontalTextSlider/index.tsx
@@ -1,26 +1,41 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/jsx-one-expression-per-line */
-import React, { useContext, useEffect, useState } from 'react';
+import React, {
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { ThemeContext } from '../../contexts';
 import './style.css';
 
 export const HorizontalTextSlider: React.FC = () => {
   const { theme } = useContext(ThemeContext);
+  const textOne = useRef<HTMLDivElement>(null);
   const [textSliderClassName1, setTextSliderClassName1] = useState('');
   const [textSliderClassName2, setTextSliderClassName2] = useState('');
+  const changeAnimationClass = () => {
+    setTextSliderClassName1('horizontal-text-slider horizontal-text-slider__two');
+  };
   useEffect(() => {
-    setTextSliderClassName1('horizontal-text-slider');
-    setTextSliderClassName2('horizontal-text-slider__two');
+    setTextSliderClassName1('horizontal-text-slider horizontal-text-slider__one');
+    setTextSliderClassName2('horizontal-text-slider horizontal-text-slider__two');
+    if (textOne.current) {
+      textOne.current.addEventListener('animationiteration', () => {
+        changeAnimationClass();
+        textOne.current?.removeEventListener('animationiteration', changeAnimationClass);
+      });
+    }
   }, []);
   return (
     <div className={`horizontal-text-slider__wrapper horizontal-text-slider__wrapper--${theme}`}>
       <div className="horizontal-text-slider__animation-wrapper">
-        <div className={textSliderClassName1}>
-          &nbsp;ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span>
+        <div ref={textOne} className={textSliderClassName1}>
+          &nbsp;ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span>
         </div>
         <div className={textSliderClassName2}>
-          &nbsp;ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span>
+          &nbsp;ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span> ux/ui <span className={`horizontal-text-slider__designer horizontal-text-slider__designer--${theme}`}>designer</span>
         </div>
       </div>
     </div>
